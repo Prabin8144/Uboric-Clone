7325c8635976e101a6b4675fb72492cc954f24b6
K PRABINA CHOUDHURY <99638485+Prabin8144@users.noreply.github.com>
2022-06-01 19:06:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+// import {Home} from './component/HomePage/Home.jsx'
+import MainRoutes from './component/Navbar/MainRoutes'
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoutes/>
+       {/* <Home/> */}
+     {/*  dvsdsdg*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/HomePage/Slide.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./slide.css";
+const colors = [
+  "https://www.uboric.com/wp-content/uploads/2021/11/grocery.jpg",
+  "https://www.uboric.com/wp-content/uploads/2021/11/health-care.jpg",
+  "https://www.uboric.com/wp-content/uploads/2021/11/clothing.jpg",
+];
+const delay = 3000;
+// "#0088FE", "#00C49F", "#FFBB28"
+export function Slide() {
+  const [index, setIndex] = React.useState(0);
+
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, index) => (
+          <div className="slide" key={index} style={{ backgroundColor }}>
+            <img src={backgroundColor} />
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="slideshowDots">
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          > */}
+      {/* <img src="" alt="" /> */}
+      {/* </div>
+        ))}
+      </div> */}
+    </div>
+  );
+}
